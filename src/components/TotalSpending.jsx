aea81fb160399f3334c2f18e499d78c6761c5d6c
haoz20 <zhaonianhao2005@gmail.com>
2025-07-26 22:30:12 +0700

    implement daily, weekly, monthly total spending

--- src/components/TotalSpending.jsx
+++ src/components/TotalSpending.jsx
@@ -1,4 +1,4 @@
-
+import { BreakfastDining } from '@mui/icons-material';
 import { use } from 'react';
 import { useState, useRef, useEffect } from 'react';
 import { useLocalStorage } from 'react-use';
@@ -15,12 +15,67 @@ function TotalSpending({ data }) {
         console.log('Selected Timeframe:', e.target.value);
     }
 
+    const weekOfYear = (date) => {
+        const startOfYear = new Date(date.getFullYear(), 0, 1);
+        startOfYear.setDate(startOfYear.getDate() + (startOfYear.getDay() % 7));
+        return Math.round((date - startOfYear) / (7 * 24 * 3600 * 1000));
+    };
+
     useEffect(() => {
         switch (timeframe) {
+            case 'daily':
+                const groupByDay = spendingData.reduce((byDay, item) => {
+                    const date = new Date(item.date).toISOString().split('T')[0];
+                    const category = item.category;
+                    if (!byDay[date]) {
+                        byDay[date] = {
+                            categories: {},
+                            total: 0
+                        };
+                    };
+                    if (!byDay[date].categories[category]) {
+                        byDay[date].categories[category] = {
+                            total: 0,
+                            items: []
+                        }
+                    }
+                    byDay[date].categories[category].total += item.amount;
+                    byDay[date].categories[category].items.push(item);
+                    byDay[date].total += item.amount;
+                    return byDay;
+                }, {});
+                setFilteredData(Object.entries(groupByDay));
+                break;
+            case 'weekly':
+                const groupByWeek = spendingData.reduce((byWeek, item) => {
+                    const date = new Date(item.date);
+                    const week = weekOfYear(date);
+                    const year = date.getFullYear();
+                    const category = item.category;
+                    const key = `${year}-Week${week}`;
+                    if (!byWeek[key]) {
+                        byWeek[key] = {
+                            categories: {},
+                            total: 0
+                        };
+                    };
+                    if (!byWeek[key].categories[category]) {
+                        byWeek[key].categories[category] = {
+                            total: 0,
+                            items: []
+                        }
+                    }
+                    byWeek[key].categories[category].total += item.amount;
+                    byWeek[key].categories[category].items.push(item);
+                    byWeek[key].total += item.amount;
+                    return byWeek;
+                }, {});
+                setFilteredData(Object.entries(groupByWeek));
+                break;
             case 'monthly':
                 const groupByMonth = spendingData.reduce((byMonth, item) => {
                     const date = new Date(item.date);
-                    const month = new Intl.DateTimeFormat("en-US", { month: 'long' }).format(date); /* things to note */
+                    const month = new Intl.DateTimeFormat("en-US", { month: 'long', year: 'numeric' }).format(date); /* things to note */
                     const category = item.category;
                     if (!byMonth[month]) {
                         byMonth[month] = {
